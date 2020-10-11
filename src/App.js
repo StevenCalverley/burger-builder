d8d519852a0fb3babf0fe6663ee6e249523417e2
Steve Calverley <steve.calverley@heffron.com.au>
2020-10-11 13:37:37 +1100

    Add Layout - add Layout component - add Aux HOC

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
-  return <div>Burger Builder</div>;
+  return (
+    <div>
+      <Layout>
+        <h1>Burger Builder</h1>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
